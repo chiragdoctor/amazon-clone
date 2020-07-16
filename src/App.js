1ccd6866cdab4a845c512e68c0b6afba462dedfa
chiragdoctor <chirag.doctor@gmail.com>
2020-07-16 17:25:20 +0100

    Banner Sub Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -9,14 +10,15 @@ function App() {
       <div className='app'>
         <Switch>
           <Route path='/checkout'>
+            <Header />
             <h1>Chekcout Page</h1>
           </Route>
           <Route path='/login'>
             <h1>Login Page</h1>
           </Route>
           <Route path='/'>
-            <Header></Header>
-            <h1>Home Page!!!</h1>
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </div>
