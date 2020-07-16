bdf65978f35f3e92af93489c39df35c8df5fea70
chiragdoctor <chirag.doctor@gmail.com>
2020-07-16 20:31:43 +0100

    Add Context API & Add to Basket action

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import './Header.css';
+import { useStateValue } from './StateProvider';
 
 const Header = () => {
+  const [{ basket }] = useStateValue();
   return (
     <nav className='header'>
       <Link to='/'>
@@ -36,7 +38,7 @@ const Header = () => {
         <Link to='/checkout' className='header__link'>
           <div className='header__optionBasket'>
             <ShoppingBasketIcon />
-            <span className='header__optionLineTwo header__basketCount'>0</span>
+            <span className='header__optionLineTwo header__basketCount'>{basket.length}</span>
           </div>
         </Link>
       </div>
---
+++ src/reducer.js
@@ -0,0 +1,26 @@
+export const initialState = {
+  basket: [],
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case 'ADD_TO_BASKET':
+      //Logic for adding items to basket
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+
+    case 'REMOVE_FROM_BASKET':
+      // Logic to remove items from basket
+      return {
+        ...state,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
